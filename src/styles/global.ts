578cc83b1139632edc2497fd687bbd34b721275c
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-04-02 00:36:49 -0300

    Arrumado a identação correta (1 tab) para o globa.ts fazer funcionar o styled-components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,57 +1,56 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-:root {
-  --background: #f0f2f5;
-  --red: #E52E4D;
-  --blue: #5429CC;
-  --blue-light: #6933FF;
-  --text-title: #363F5F;
-  --text-body: #969CB3;
-  --background: #F0F2F5;
-  --shape: #FFFFFF;
-}
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-// font-size: 16px; // tamanho padrao para desktop
-html {
-  @media (max-width: 1080px) {
-    font-size: 93.75%; // 16px x 0,9375 = 15 pixels
+  :root {
+    --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
-  @media (max-widht: 720px) {
-    font-size: 87.5%; // 15x 0,875 = 14 pixels
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
-}
 
-body, input, textarea, button {
-  font-family: 'Poppins', sans-serif;
-  font-weight: 400; // tamanho padrao dela e no html = 500
-}
+  // font-size: 16px; // tamanho padrao para desktop
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 16px x 0,9375 = 15 pixels
+    }
+    @media (max-widht: 720px) {
+      font-size: 87.5%; // 15x 0,875 = 14 pixels
+    }
+  }
 
-h1, h2, h3, h4, h5, h6, strong {
-  font-weight: 600;
-}
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400; // tamanho padrao dela e no html = 500
+  }
 
-// REM = 1rem = font-size = 15 pixes ou 14 pixels dependendo do dispositivo
-// ótima prática, pois tudo se adaptará ao tamanho do dispositivo
-// o legal de usar % ao invés de pixel direto, vai influenciar o lance do tamanho da fonte que o usuário vai utilizar
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
 
-body {
-  background: var(--background);
-  -webkit-font-smoothing: antialiased; //deixa as fontes nítidas
-}
+  // REM = 1rem = font-size = 15 pixes ou 14 pixels dependendo do dispositivo
+  // ótima prática, pois tudo se adaptará ao tamanho do dispositivo
+  // o legal de usar % ao invés de pixel direto, vai influenciar o lance do tamanho da fonte que o usuário vai utilizar
 
-button {
-  cursor: pointer;
-}
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased; //deixa as fontes nítidas
+  }
 
-[disabled] {
-  opacity: 0.6;
-  cursor: not-allowed; // placa de nao-permitido
-}
+  button {
+    cursor: pointer;
+  }
 
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed; // placa de nao-permitido
+  }
 `
